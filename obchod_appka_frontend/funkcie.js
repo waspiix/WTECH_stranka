12012e3bdb9dea184e19f50a94a6ef3ea34cf7c6
waspiix <t.horan8777@gmail.com>
2025-03-17 16:43:59 +0100

    Inicializacia projektu w/ frontend, backend

---
+++ obchod_appka_frontend/funkcie.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-container").innerHTML = data;
+        })
+        .catch(error => console.error("Chyba pri načítaní headeru:", error));
+});
+
+
+// Funkcia na nastavenie kategórie podľa pohlavia
+function setGenderCategory(category) {
+    localStorage.setItem('selectedGenderCategory', category);
+    if (category === 'zeny') {
+        window.location.href = 'zeny.html';
+    } else if (category === 'muzi') {
+        window.location.href = 'muzi.html'; 
+    } else if (category === 'deti') {
+        window.location.href = 'deti.html';
+    }
+}
+
+
+
