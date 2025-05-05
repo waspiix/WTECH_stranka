cf9b9477367de6772935cba1608104a59cec2c8d
Stefan Kucerak <xkucerak@stuba.sk>
2025-05-05 15:03:54 +0200

    kosik refactor

--- obchod_appka_backend/app/Http/Controllers/KosikController.php
+++ obchod_appka_backend/app/Http/Controllers/KosikController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\KosikService;
 use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
@@ -9,50 +10,17 @@ use Illuminate\Http\Request;
 
 class KosikController extends Controller
 {
+    public function __construct(protected KosikService $kosik) {}
+
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $cena = 0;
-        $products = [];
-
-        if ($user = Auth::user()) {
-            $products = $user->products()->with('image')->get();
-        } else {
-            $kosik = session()->get('kosik', []);
-
-            $ids = array_column($kosik, 'product_id');
-
-            $products_list = Product::whereIn('id', $ids)->with('image')->get()->keyBy('id');
-
-            $counter = 0;
-
-            $products = array_map(function ($kosik) use ($products_list, &$counter) {
-                $product = clone $products_list[$kosik['product_id']];
-
-                $product->pivot = (object)[
-                    'velkost' => $kosik['velkost'],
-                    'pocet' => $kosik['pocet'],
-                    'id' => $counter++
-                ];
-
-                return $product;
-            }, $kosik);
-
-
-
-            // foreach ($kosik as &$test) {
-            //     $test['pivot'] = ['velkost' => $test['velkost']];
-            // }
-        }
-
-        // return $products;
-
-
-        foreach ($products as $product) {
-            $cena += $product->cena * $product->pivot->pocet;
-        }
+        //
+        $products = $this->kosik->getProducts();
+        $cena = $this->kosik->getCena($products);
 
         return view(
             'kosik.index',
@@ -69,25 +37,7 @@ class KosikController extends Controller
     public function store(Request $request): RedirectResponse
     {
         //
-
-        $product_id = $request->id;
-        $pocet = $request->pocet;
-        $velkost = $request->velkost;
-
-
-        if ($user = Auth::user()) {
-            $product = Product::find($product_id);
-            $user->products()->attach($product->id, ['pocet' => $pocet, 'velkost' => $velkost]);
-        } else {
-            $key = $product_id . '_' . $velkost;
-            $kosik = session('kosik', []);
-            $kosik[$key] = [
-                'product_id' => $product_id,
-                'pocet' => $pocet,
-                'velkost' => $velkost
-            ];
-            session(['kosik' => $kosik]);
-        }
+        $this->kosik->addProduct($request->id, $request->pocet, $request->velkost);
 
         return redirect('/kosik');
     }
@@ -98,44 +48,7 @@ class KosikController extends Controller
     public function update(Request $request, string $id): RedirectResponse
     {
         //
-        if ($user = Auth::user()) {
-
-            $product = $user->products()->wherePivot('id', $id);
-            $pocet = $product->first()->pivot->pocet;
-
-            if ($request->action === 'inc') {
-                $pocet++;
-            } elseif ($request->action === 'dec') {
-                $pocet--;
-            }
-
-            if ($pocet < 1) {
-                $product->detach();
-            } else {
-                $product->first()->pivot->update(['pocet' => $pocet]);
-            }
-        } else {
-            $kosik = session()->get('kosik', []);
-
-            $key = array_keys($kosik)[$id];
-
-            $pocet = $kosik[$key]['pocet'];
-
-            if ($request->action === 'inc') {
-                $pocet++;
-            } elseif ($request->action === 'dec') {
-                $pocet--;
-            }
-
-
-            if ($pocet < 1) {
-                array_splice($kosik, $id, 1);
-            } else {
-                $kosik[$key]['pocet'] = $pocet;
-            }
-
-            session(['kosik' => $kosik]);
-        }
+        $this->kosik->updateProduct($id, $request->action);
 
         return redirect('/kosik');
     }
@@ -146,13 +59,7 @@ class KosikController extends Controller
     public function destroy(string $id): RedirectResponse
     {
         //
-        if ($user = Auth::user()) {
-            $user->products()->wherePivot('id', $id)->detach();
-        } else {
-            $kosik = session()->get('kosik', []);
-            array_splice($kosik, $id, 1);
-            session(['kosik' => $kosik]);
-        }
+        $this->kosik->removeProduct($id);
 
         return redirect('/kosik');
     }
---
+++ obchod_appka_backend/app/Services/KosikService.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+
+class KosikService
+{
+    public function getProducts()
+    {
+        $products = [];
+
+        if ($user = Auth::user()) {
+            $products = $user->products()->with('image')->get();
+        } else {
+            $kosik = session()->get('kosik', []);
+
+            $ids = array_column($kosik, 'product_id');
+
+            $products_list = Product::whereIn('id', $ids)->with('image')->get()->keyBy('id');
+
+            $counter = 0;
+
+            $products = array_map(function ($kosik) use ($products_list, &$counter) {
+                $product = clone $products_list[$kosik['product_id']];
+
+                $product->pivot = (object)[
+                    'velkost' => $kosik['velkost'],
+                    'pocet' => $kosik['pocet'],
+                    'id' => $counter++
+                ];
+
+                return $product;
+            }, $kosik);
+        }
+
+        return $products;
+    }
+
+    public function getCena($products)
+    {
+        $cena = 0;
+        foreach ($products as $product) {
+            $cena += $product->cena * $product->pivot->pocet;
+        }
+        return $cena;
+    }
+
+    public function addProduct($product_id, $pocet, $velkost)
+    {
+        if ($user = Auth::user()) {
+            $product = Product::find($product_id);
+            $user->products()->attach($product->id, ['pocet' => $pocet, 'velkost' => $velkost]);
+        } else {
+            $key = $product_id . '_' . $velkost;
+            $kosik = session('kosik', []);
+            $kosik[$key] = [
+                'product_id' => $product_id,
+                'pocet' => $pocet,
+                'velkost' => $velkost
+            ];
+            session(['kosik' => $kosik]);
+        }
+    }
+
+    function updateProduct($id, $action)
+    {
+        if ($user = Auth::user()) {
+
+            $product = $user->products()->wherePivot('id', $id);
+            $pocet = $product->first()->pivot->pocet;
+
+            if ($action === 'inc') {
+                $pocet++;
+            } elseif ($action === 'dec') {
+                $pocet--;
+            }
+
+            if ($pocet < 1) {
+                $product->detach();
+            } else {
+                $product->first()->pivot->update(['pocet' => $pocet]);
+            }
+        } else {
+            $kosik = session()->get('kosik', []);
+
+            $key = array_keys($kosik)[$id];
+
+            $pocet = $kosik[$key]['pocet'];
+
+            if ($action === 'inc') {
+                $pocet++;
+            } elseif ($action === 'dec') {
+                $pocet--;
+            }
+
+
+            if ($pocet < 1) {
+                array_splice($kosik, $id, 1);
+            } else {
+                $kosik[$key]['pocet'] = $pocet;
+            }
+
+            session(['kosik' => $kosik]);
+        }
+    }
+    function removeProduct($id)
+    {
+        if ($user = Auth::user()) {
+            $user->products()->wherePivot('id', $id)->detach();
+        } else {
+            $kosik = session()->get('kosik', []);
+            array_splice($kosik, $id, 1);
+            session(['kosik' => $kosik]);
+        }
+    }
+}
